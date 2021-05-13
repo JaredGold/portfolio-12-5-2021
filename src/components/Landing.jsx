3f8f7f511858dfa949b1c3561ba4b12018697433
JaredGold <jegoldstein94@gmail.com>
2021-05-13 19:47:40 +1000

    Add more styles

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <p className="h-screen">
-      Hello I am <span>Jared Goldstein</span><br/>
+    <p className="h-screen bg-gray-700 text-gray-200 text-center text-4xl">
+      Hello I am <span className="text-pink-400">Jared Goldstein</span><br/>
       I am full-stack developer.
     </p>
   )
