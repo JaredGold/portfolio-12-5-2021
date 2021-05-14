1e1410e933d39ed0843c8aa4e6004149e9ded79c
JaredGold <jegoldstein94@gmail.com>
2021-05-14 18:05:13 +1000

    Clean code

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Canvas from './Canvas'
 
 const Landing = () => {
+  
+
   return (
-    <p className="h-screen bg-gray-700 text-gray-200 text-center text-4xl">
-      Hello I am <span className="text-pink-400">Jared Goldstein</span><br/>
-      I am full-stack developer.
-    </p>
+    <div className="h-screen bg-green-900 w-100%">
+      <Canvas className="absolute inset-0 z-10"/>
+      <p className="text-gray-200 text-center text-4xl bg-green-900 z-50">
+        Hello I am <span className="text-pink-400">Jared Goldstein</span><br/>
+        I am full-stack developer.
+      </p>
+    </div>
   )
 }
 
