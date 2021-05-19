38ac3b8063813d29ea28a6348acd53f4dbd96ea6
JaredGold <jegoldstein94@gmail.com>
2021-05-19 17:11:14 +1000

    Add project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Project = (props) => {
+
+  return(
+    <div className="bg-white rounded-md flex w-96 flex-col p-4 shadow-2xl" >
+      <img src={props.imageSource} alt="Random Temp" className="rounded-t-md self-center" />
+      <h2 className="text-2xl text-center font-medium pt-2">{props.title}</h2>
+      <p className="text-center">{props.description}</p>
+      <h3 className="text-xl text-center font-medium pt-3">Frameworks Used</h3>
+      <ul className="text-center">
+        {props.frameworks.map((lang) => <li>{lang}</li>)}
+      </ul>
+      <a href={props.linkToWork} className="text-center pt-4 text-purple-600">Link To Project</a>
+    </div>
+  )
+}
+
+export default Project
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade';
+import Project from './Project'
 
 const Projects = () => {
   return(
     <div className="h-screen bg-gray-100 w-100% flex justify-evenly flex-wrap gap-8 items-center" id="projects">
       <Fade left>
-        <div className="bg-white rounded-md flex w-96 flex-col p-4 shadow-2xl" >
-          <img src="https://picsum.photos/400/300" alt="Random Temp" className="rounded-t-md self-center" />
-          <h2 className="text-2xl text-center font-medium pt-2">Project Title</h2>
-          <p className="text-center">This project was done while I was studying my course.
-            In this I was forced to learn how to use these very interesting languages.
-            I used these languages as such and it made me level up.
-          </p>
-          <h3 className="text-xl text-center font-medium pt-3">Frameworks Used</h3>
-          <ul className="text-center">
-            <li>Lang 1</li>
-            <li>Lang 2</li>
-            <li>Lang 3</li>
-          </ul>
-          <a href="/" className="text-center pt-4 text-purple-600">Link To Project</a>
-        </div>
+        <Project 
+          title="Project Title" 
+          description="This project was done while I was studying my course.
+          In this I was forced to learn how to use these very interesting languages.
+          I used these languages as such and it made me level up."
+          imageSource="https://picsum.photos/400/300"
+          frameworks={["HTML", "CSS", "React.js"]}
+          linkToWork="/"
+        />
       </Fade>
       
     </div>
